36c0a23f06ae88e203e7c8c3218d2dbb3e93215e
Josh12903 <hank76421@gmail.com>
2026-02-11 01:34:46 +0800

    260211 基本畫面配置

---
+++ api/db.php
@@ -0,0 +1,61 @@
+<?php
+
+session_start();
+
+Class DB{
+    $dsn="mysql:host=localhost;dbname=db04;charset:ubf8";
+    $pdo=new PDO($dsn,'root','');
+    $table->this;
+
+
+    function __construct()
+
+    
+
+    function all(){
+        $sql=" SELECT * FROM ";
+
+        return $this->table;
+    }
+    function save(){
+        $sql=" SELECT * FROM ";
+
+        return $this->table;
+    }
+    function find(){
+        $sql=" SELECT * FROM ";
+
+        return $this->table;
+    }
+    function del(){
+        $sql=" SELECT * FROM ";
+
+        return $this->table;
+    }
+    function count(){
+        $sql=" SELECT * FROM ";
+
+        return $this->table;
+    }
+    function sum(){
+        $sql=" SELECT * FROM ";
+
+        return $this->table;
+    }
+    function (){
+        $sql=" SELECT * FROM ";
+
+        return $this->table;
+    }
+
+}
+
+function dd(){
+    
+}
+
+function q(){
+    $dsn="mysql:host=localhost;dbname=db04;charset:ubf8";
+    $pdo=new PDO($dsn,'root','');
+    $table->this;
+}
