b3ef2d6f89386f75f8721bb2e8cd3d58900bb3be
Josh12903 <hank76421@gmail.com>
2026-02-18 02:06:26 +0800

    商品版面至詳述；初步代參數到購物車

--- front/main.php
+++ front/main.php
@@ -4,13 +4,47 @@ if(isset($_GET['type']) && $_GET['type']!=0){
     $type=$Type->find($_GET['type']);
     if($type['big_id']==0){
         $nav_str=$type['name'];
+            $rows=$Item->all(['sh'=>1,'big'=>$type['id']]);
     }else{
         $big=$Type->find($type['big_id']);
         $nav_str=$big['name'] . " > " . $type['name'];
+            $rows=$Item->all(['sh'=>1,'mid'=>$type['id']]);
     }
 }else{
 $nav_str="全部商品";
+    $rows=$Item->all(['sh'=>1]);
 }
 ?>
 
 <h2><?=$nav_str;?></h2>
+<?php
+foreach($rows as $row):
+?>
+
+<div class="pp" style="display:flex;width:70%;margin:2px auto">
+    <div class="pp ct" style="width:40%;padding:10px;border:1px solid white">
+        <a href="?do=detail&id=<?=$row['id'];?>">
+        <img src="pic/<?=$row['img'];?>" style="width:150px;height:120px">
+        </a>
+    </div>
+    <div style="width:60%">
+        <div class="ct tt" style="border:1px solid white"><?=$row['name'];?></div>
+        <div class="pp" style="padding:5px 2px;border:1px solid white">
+            價格:<?=$row['price'];?>
+            <a href="?=buycart&id=<?=$row['id'];?>&stock=1" style="float:right">
+            <img src="icon/0402.jpg" alt="">
+            </a>
+        </div>
+        <div class="pp" style="padding:5px 2px;border:1px solid white">
+            規格:<?=$row['spec'];?>
+        </div>
+        <div class="pp" style="padding:5px 2px;border:1px solid white">
+            簡介:<?=mb_substr($row['intro'],0,20);?>...
+        </div>
+    </div>
+</div>
+
+
+<?php
+endforeach;
+?>
